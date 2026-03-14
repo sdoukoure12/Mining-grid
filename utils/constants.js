c179652218896ea943d4bee8fd2ae918cf7a81e4
Si <sdoukoure12@gmail.com>
2026-03-14 18:31:05 +0100

    Create utils/constants.js

---
+++ utils/constants.js
@@ -0,0 +1,8 @@
+// mining-grid-backend/utils/constants.js
+
+module.exports = {
+  STATUS_ENUM: ['active', 'inactive', 'warning', 'restarting'],
+  DEFAULT_POWER_LIMIT: 3500,
+  TEMP_THRESHOLD_WARNING: 70,
+  TEMP_THRESHOLD_CRITICAL: 85,
+};
